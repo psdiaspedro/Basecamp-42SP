d24ea59824024c4b5ff55f7cc9349e726ad0a9e3
pedroadias <psdias.pedro@gmail.com>
2021-08-01 20:46:22 -0300

    finalizando a c02

---
+++ c02/ex10/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+
+int	main(void)
+{
+	int 	src_size;
+	char 	*src;
+	char 	*dest;
+
+	src = calloc(11, sizeof(char));
+	dest = calloc(8, sizeof(char));
+	src = "alo galera";
+	src_size = ft_strlcpy(dest, src, 8);
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+	printf("src_size: %d\n", src_size);
+	return(0);
+}
